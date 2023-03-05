15bb113590a5608763190031dce909223ab9ddcc
lcmDev97 <lcmdev97@gmail.com>
2023-03-05 17:13:39 +0900

    .gitignore 적용"

--- dist/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({
-    server_port: parseInt(process.env.PORT),
-});
-//# sourceMappingURL=config.js.map
