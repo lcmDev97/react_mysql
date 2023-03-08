7269fa6e0bfb514a00fcc108588c5f4923a93fc1
lcmDev97 <lcmdev97@gmail.com>
2023-03-08 15:16:13 +0900

    [update] cors옵션 추가 (react서버에서 로그인시 브라우저에 토큰 안들어오는 문제 해결)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: true,
+    credentials: true
+  })
   app.use(cookieParser())
   await app.listen(process.env.SERVER_PORT);
 }
