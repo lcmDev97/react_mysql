59706b16660597c16a2a3330b7bd5fca0f1945e3
lcmDev97 <lcmdev97@gmail.com>
2023-03-14 19:31:14 +0900

    [server][fix] passport jwt얻는곳을 cookie로부터 얻도록 설정
    httponly이므로, header에 포함해 리액트가 요청한다는게 말이 안되기에 설정하였습니다.

--- Server/src/auth/security/passport.jwt.strategy.ts
+++ Server/src/auth/security/passport.jwt.strategy.ts
@@ -1,15 +1,23 @@
 import { HttpException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
 import { ExtractJwt, Strategy, VerifiedCallback } from "passport-jwt";
 import { AuthService } from "../auth.service";
 import { Payload } from "./payload.interface";
-
+const cookieExtractor = function (req: Request) {
+    let token = null;
+    if (req && req.cookies) {
+      token = req.cookies['jwt'];
+    }
+    return token;
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private authService: AuthService){
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: cookieExtractor,
             ignoreExpiration: true,
             secretOrKey: process.env.JWT_SECRET_KEY,
         })
