e739bc9c51d4aa5fba7dec00e14189664dad97f7
lcmDev97 <lcmdev97@gmail.com>
2023-03-15 16:02:17 +0900

    [server][update] http-exception.filter grobal 적용

---
+++ Server/src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();   //<= 실행환경(context)에 대한 설정
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    let message
+    if(status === 404) {
+        message = "404 ERROR :("
+    } else {
+        message = exception.getResponse()
+    }
+
+    response.status(status)
+      .json({
+        success: false,
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: message
+      });
+  }
+}
--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     credentials: true
   })
   app.use(cookieParser())
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
