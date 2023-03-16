876b1b5a302fe0180d869004361823d7963b14ce
lcmDev97 <lcmdev97@gmail.com>
2023-03-16 17:34:33 +0900

    [server][update] success.interceptor 생성 및 grobal 적용

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   })
   app.use(cookieParser())
   app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalInterceptors(new SuccessInterceptor())
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
